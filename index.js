167e0e1b7e935f674c2bf2c56e8eb8762bcc6200
Saeed Hassani <saeedh.code@gmail.com>
2024-02-16 08:51:47 -0800

    ♻️ Refactor: refactor bot codes

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// Import necessary libraries
 const { Telegraf, Markup } = require('telegraf');
 const moment = require('moment');
 
-// dotenv
+// Load environment variables
 require("dotenv").config()
 
+// Telegram API token
 const TOKEN = process.env.TELEGRAM_API_TOKEN
+
+// Initialize the bot with the API token
 const bot = new Telegraf(TOKEN, {
-    'telegram': {
-        'apiRoot': 'https://public-telegram-bypass.solyfarzane9040.workers.dev/bot'
+    telegram: {
+        apiRoot: 'https://public-telegram-bypass.solyfarzane9040.workers.dev/bot'
     }
 });
+
+// Initial message text and target date
 const messageText = "برای مشاهده زمان باقی مانده تا شروع همایش روی دکمه زیر کلیک کنید.";
 const targetDate = moment("2024-02-29");
 
+// Function to calculate remaining time
 function calculateRemainingTime() {
     const now = moment();
     const remainingTime = moment.duration(targetDate.diff(now));
@@ -22,6 +29,7 @@ function calculateRemainingTime() {
     return `${days} روز، ${hours} ساعت و ${minutes} دقیقه`;
 }
 
+// Command to start the bot
 bot.start((ctx) => {
     const remainingTime = calculateRemainingTime();
     ctx.reply(`🥕 سلام به فرانت چپتری های عزیز\n${messageText}`, Markup.inlineKeyboard([
@@ -29,9 +37,11 @@ bot.start((ctx) => {
     ]));
 });
 
+// Action to display remaining time
 bot.action('showRemainingTime', (ctx) => {
     const remainingTime = calculateRemainingTime();
     ctx.answerCbQuery(`🥕 زمان باقی‌مانده: ${remainingTime}`);
 });
 
+// Launch the bot
 bot.launch();
